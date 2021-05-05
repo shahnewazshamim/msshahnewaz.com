7510b0cc50e4f6e1f6fa3d0df65c6b1c2a3b14b5
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2021-05-06 05:58:21 +0600

    Instagram functionality added

--- template-parts/content-instagram.php
+++ template-parts/content-instagram.php
@@ -8,76 +8,19 @@
  */
 ?>
 
-<!-- Gallery -->
-<section class="jophoto-gallery section-padding">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 mb-30">
-                <span class="section-subtitle">Portfolio</span>
-                <h2 class="section-title">Gallery</h2>
+<?php if(cs_get_options()['mss_home_is_instagram']): ?>
+    <!-- Instagram -->
+    <section class="jophoto-gallery section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mb-30">
+                    <span class="section-subtitle">Instagram</span>
+                    <h2 class="section-title">Gallery</h2>
+                </div>
+            </div>
+            <div class="row jophoto-photos" id="jophoto-section-photos">
+                <?php echo do_shortcode( '[instagram-feed]' ); ?>
             </div>
         </div>
-        <div class="row jophoto-photos" id="jophoto-section-photos">
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/01.jpg" class="d-block jophoto-photo-item" data-caption="Portrait Photo" data-fancybox="gallery"> <img src="img/gallery/01.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/02.jpg" class="d-block jophoto-photo-item" data-caption="Wedding Photo" data-fancybox="gallery"> <img src="img/gallery/02.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/03.jpg" class="d-block jophoto-photo-item" data-caption="Portrait Photo" data-fancybox="gallery"> <img src="img/gallery/03.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/04.jpg" class="d-block jophoto-photo-item" data-caption="Portrait Photo" data-fancybox="gallery"> <img src="img/gallery/04.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/05.jpg" class="d-block jophoto-photo-item" data-caption="Portrait Photo" data-fancybox="gallery"> <img src="img/gallery/05.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/06.jpg" class="d-block jophoto-photo-item" data-caption="Fashion Photo" data-fancybox="gallery"> <img src="img/gallery/06.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/07.jpg" class="d-block jophoto-photo-item" data-caption="Travel Photo" data-fancybox="gallery"> <img src="img/gallery/07.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/08.jpg" class="d-block jophoto-photo-item" data-caption="Wedding Photo" data-fancybox="gallery"> <img src="img/gallery/08.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/09.jpg" class="d-block jophoto-photo-item" data-caption="Personal Photo" data-fancybox="gallery"> <img src="img/gallery/09.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/10.jpg" class="d-block jophoto-photo-item" data-caption="Wedding Photo" data-fancybox="gallery"> <img src="img/gallery/10.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/11.jpg" class="d-block jophoto-photo-item" data-caption="Baby Photo" data-fancybox="gallery"> <img src="img/gallery/11.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-            <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
-                <a href="img/gallery/12.jpg" class="d-block jophoto-photo-item" data-caption="Sport Photo" data-fancybox="gallery"> <img src="img/gallery/12.jpg" alt="Image" class="img-fluid">
-                    <div class="photo-text-more"> <span class="ti-fullscreen"></span> </div>
-                </a>
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
