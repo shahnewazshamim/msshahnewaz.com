619a438caa10d901b156ad97d1b0058866b5149e
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2021-05-06 05:58:40 +0600

    Bio on home page functionality added

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -8,34 +8,37 @@
  */
 ?>
 
-<!-- About Us -->
-<section class="proto-services section-padding">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 mb-30">
-                <span class="section-subtitle">About Us</span>
-                <h2 class="section-title">Jopho</h2>
+<?php if(cs_get_options()['mss_home_is_bio']): ?>
+    <!-- About Us -->
+    <section class="proto-services section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mb-30">
+                    <span class="section-subtitle"><?php echo cs_get_options()['mss_header']['mss_header_tagline'] ?> and Indie Filmmaker</span>
+                    <h2 class="section-title"><?php echo cs_get_options()['mss_header']['mss_header_site_name'] ?></h2>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-7">
-                <div class="row">
-                    <div class="col-md-3 animate-box" data-animate-effect="fadeInUp">
-                        <span class="line-one"></span>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="row">
+                        <div class="col-md-3 animate-box" data-animate-effect="fadeInUp">
+                            <span class="line-one"></span>
+                        </div>
+                        <div class="col-md-8 animate-box mb-60" data-animate-effect="fadeInUp">
+                            <h6><?php echo cs_get_options()['mss_bio_intro'] ?></h6>
+                            <?php foreach(cs_get_options()['mss_bio_repeater'] as $data) : ?>
+                                <p class="text-justify"><?php echo $data['mss_bio_repeater_para'] ?></p>
+                            <?php endforeach; ?>
+                            <img src="<?php echo cs_get_options()['mss_misc']['mss_misc_signature']['url'] ?>" class="sign" alt="<?php echo cs_get_options()['mss_header']['mss_header_site_name'] ?>">
+                        </div>
                     </div>
-                    <div class="col-md-8 animate-box mb-60" data-animate-effect="fadeInUp">
-                        <h6>Photography bibendu tunc duntez varius. Vestibulum viverra the mana mattis gravida sapien</h6>
-                        <p>Shots eu nunc bibe endum in finibus elit eget the solli citudin elit. Phasellus rutrum in lacus ut euismod. Vestibulum eleifend tortor orci eu ornare tortor semper at. Duis scelerisque the nibhse drana moss pulvinar laoreet.</p>
-                        <p>Vivense eu nunc bibe endum in finibus elit eget the solli citudin elit. Phasellus rutrum in lacus ut euismod. Vestibulum eleifend tortor orci eu ornare tortor semper at. Duis scelerisque the nibhse drana moss pulvinar laoreet.</p>
-                        <img src="img/sign.png" class="sign" alt="">
+                </div>
+                <div class="col-md-5 animate-box" data-animate-effect="fadeInUp">
+                    <div class="jophoto-about-img">
+                        <div class="img"> <img src="<?php echo cs_get_options()['mss_misc']['mss_misc_profile']['url'] ?>" alt="<?php echo cs_get_options()['mss_header']['mss_header_site_name'] ?>"> </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-5 animate-box" data-animate-effect="fadeInUp">
-                <div class="jophoto-about-img">
-                    <div class="img"> <img src="img/about.jpg" alt=""> </div>
-                </div>
-            </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
