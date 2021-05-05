9baef9bf93e9a5792a621d73a9a4ec3c5deaa7f2
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2021-05-06 05:58:05 +0600

    loading condition added for all page

--- template-parts/content-loading.php
+++ template-parts/content-loading.php
@@ -8,14 +8,16 @@
  */
 ?>
 
-<!-- Loading -->
-<div class="loading">
-    <div class="text-center middle">
-        <div class="lds-ellipsis">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+<?php if(cs_get_options()['mss_misc']['mss_misc_is_loading']): ?>
+    <!-- Loading -->
+    <div class="loading">
+        <div class="text-center middle">
+            <div class="lds-ellipsis">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     </div>
-</div>
+<?php endif; ?>
