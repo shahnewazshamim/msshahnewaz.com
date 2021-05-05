c0e5a6731318e83d1708cded40bbaec31099685e
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2021-05-06 04:01:15 +0600

    misc

--- page-template/mss-home.php
+++ page-template/mss-home.php
@@ -10,7 +10,7 @@
 
 <?php get_header(); ?>
 <?php //get_template_part( 'template-parts/content', 'loading' ); ?>
-<?php //get_template_part( 'template-parts/content', 'navbar' ); ?>
+<?php get_template_part( 'template-parts/content', 'navbar' ); ?>
 <?php get_template_part( 'template-parts/content', 'slider' ); ?>
 <?php get_template_part( 'template-parts/content', 'about' ); ?>
 <?php get_template_part( 'template-parts/content', 'instagram' ); ?>
