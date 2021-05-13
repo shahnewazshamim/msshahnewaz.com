a026b25324091d4347e3b44ff88f1da5e897d95f
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2021-05-13 07:31:15 +0600

    Others page implemented with header

---
+++ page-contents/main-singles.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template part (page) for displaying singles page main content.
+ *
+ * @package    WordPress
+ * @subpackage M_S_Shahnewaz_Dark
+ * @since      M S Shahnewaz 1.0.1
+ */
+?>
+
--- page-template/mss-tearsheet.php
+++ page-template/mss-tearsheet.php
@@ -11,6 +11,7 @@
 <?php get_header(); ?>
 <?php get_template_part( 'template-parts/content', 'loading' ); ?>
 <?php get_template_part( 'template-parts/content', 'navbar' ); ?>
-
+<?php get_template_part( 'template-parts/content', 'banner' ); ?>
+<?php get_template_part( 'page-contents/main', 'tearsheet' ); ?>
 <?php get_template_part( 'template-parts/content', 'footer' ); ?>
 <?php get_footer(); ?>
