6123e6d064ec558ced173217101388d7ab6065a6
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2021-06-01 10:25:13 +0600

    Film page added

---
+++ page-template/mss-films.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: MSS Portfolio
+ *
+ * @package    WordPress
+ * @subpackage M_S_Shahnewaz_Dark
+ * @since      M S Shahnewaz 1.0.1
+ */
+?>
+
+<?php get_header(); ?>
+<?php get_template_part( 'template-parts/content', 'loading' ); ?>
+<?php get_template_part( 'template-parts/content', 'navbar' ); ?>
+<?php get_template_part( 'template-parts/content', 'banner' ); ?>
+<?php get_template_part( 'page-contents/main', 'portfolio' ); ?>
+<?php get_template_part( 'template-parts/content', 'footer' ); ?>
+<?php get_footer(); ?>
